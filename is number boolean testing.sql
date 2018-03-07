de7b49adb37a744fb9c2974df3d6674e01220132
Dobrivoje Stankovic <dobri.stankovic@gmail.com>
2018-03-07 14:16:20 +0100

    Add files via upload

---
+++ is number boolean testing.sql
@@ -0,0 +1,24 @@
+DECLARE
+   retval      BOOLEAN;
+   string_in   VARCHAR2 (32767);
+   FUNCTION bool2char (bool IN BOOLEAN)
+      RETURN VARCHAR2
+   IS
+   BEGIN
+      IF bool
+      THEN
+         RETURN 'TRUE';
+      ELSIF NOT bool
+      THEN
+         RETURN 'FALSE';
+      ELSE
+         RETURN 'NULL';
+      END IF;
+   END;
+BEGIN
+   string_in := '123456789'; -- Here change the input string in order to test it
+   retval := is_number_boolean_2 (string_in);
+   DBMS_OUTPUT.put_line ('RetVal = ' || bool2char (retval));
+   DBMS_OUTPUT.put_line ('');
+   COMMIT;
+END;
